5c98c575cdf04fd0239f33bc96410d09dc5eafaa
morning-1 <morning-1@admins-iMac.local>
2022-10-14 10:09:12 +0800

    first commit

---
+++ Eriksen_files/config(1).js
@@ -0,0 +1,6 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C6N1LPLH2V1U663IR550").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true,"auto_email":true,"auto_phone_number":true});
+window[window["TiktokAnalyticsObject"]].instance("C6N1LPLH2V1U663IR550").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C6N1LPLH2V1U663IR550").setPixelInfo({status: 0, name: "TikTok MUFC.com", advertiserID: "7025599298221899777", setupMode: 1, partner: "", is_onsite: false });
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("manutd.com");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
+window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie && window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie(true);
